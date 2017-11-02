882b76be1e41de760ff56d62475f68bda9595975
trinadhkoya <trinadh.koya@zaggle.in>
2017-11-02 20:19:55 +0530

    initial commit

---
+++ app/resources/styles/colors.js
@@ -0,0 +1,22 @@
+'use strict'
+const colors = {
+  txtMain: '#00a699',
+  txtMainRed: '#FF5A5F',
+  txtDescription: '#757575',
+  txtDark: '#484848',
+  txtWhite: '#ffffff',
+
+  bgMain: '#00a699',
+  bgMainDark: '#007e8c', // For screens > auth
+  bgMainRed: '#FF5A5F',
+  bgWhite: '#ffffff',
+  bgError: '#fb642d',
+  bgChat: '#f1f1f1',
+
+  bdMain: '#00a699',
+  bdWhite: '#ffffff',
+  bdLine: '#dddddd',
+  bdInput: '#cbcbcb'
+}
+
+export default colors
